2ce5127a8cf876506e41a04bdc4db62f6abf4325
Saif <saifisami2005@gmail.com>
2024-01-12 13:13:47 -0500

    Finished the exercise.

--- calc/app.py
+++ calc/app.py
@@ -1,2 +1,13 @@
-from flask import Flask
+from flask import Flask, request
+import operations
 app = Flask(__name__)
+
+@app.route("/<operation>")
+def evaluate(operation):
+    a, b = int(request.args.get("a", 0)), int(request.args.get("b", 1))
+    return str(eval(f"operations.{operation}")(a,b))
+
+@app.route("/math/<operation>")
+def evaluate2(operation):
+    a, b = int(request.args.get("a", 0)), int(request.args.get("b", 1))
+    return str(eval(f"operations.{operation}")(a,b))
